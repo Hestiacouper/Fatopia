244ef75ba4b7b6f09b6e7813af3f9772c9170564
Kevincouper <fawderfox@gmail.com>
2019-12-09 14:42:35 +0100

    IceCreamCanon rotating correctly

---
+++ Fatopia/Assets/Scripts/IceCreamCanonController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class IceCreamCanonController : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] Transform target;
+    void Start()
+    {
+       
+    }
+    void Update()
+    {
+        Vector2 direction = target.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle+90, Vector3.forward);
+        transform.rotation = rotation; // Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
+        //Debug.Log("Triggered");
+    }
+   /*private void OnTriggerEnter2D(Collider2D other)
+    {
+        Vector2 direction = target.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = rotation; // Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
+        Debug.Log("Triggered");
+    }*/
+}

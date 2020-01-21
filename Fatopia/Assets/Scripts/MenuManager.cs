6e08eaf4ffc5f875e8aa9e7acde465d7ff033934
Kevincouper <fawderfox@gmail.com>
2020-01-21 22:35:07 +0100

    Pause key changed to escape

--- Fatopia/Assets/Scripts/MenuManager.cs
+++ Fatopia/Assets/Scripts/MenuManager.cs
@@ -19,7 +19,7 @@ public class MenuManager : MonoBehaviour
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (panelMenuPause.activeSelf)
             {
